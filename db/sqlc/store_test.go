df045f546d0b7115fd1176a02282f631d196ce01
Mrityunjay Saxena <14573967+mrityunjaygr8@users.noreply.github.com>
2022-11-03 14:03:16 +0530

    store struct created for transaction support

---
+++ db/sqlc/store_test.go
@@ -0,0 +1,87 @@
+package db
+
+import (
+	"context"
+	"testing"
+
+	"github.com/mrityunjaygr8/simplebank/utils"
+	"github.com/stretchr/testify/require"
+)
+
+func TestTransferTx(t *testing.T) {
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+
+	store := NewStore(testDb)
+
+	errs := make(chan error)
+	results := make(chan TransferTxResult)
+
+	n := 5
+	amount := int64(utils.RandomMoney())
+
+	for x := 0; x < n; x++ {
+		go func() {
+			transfer, err := store.TransferTx(context.Background(), TransferTxParams{
+				FromAccountID: account1.ID,
+				ToAccountID:   account2.ID,
+				Amount:        amount,
+			})
+
+			errs <- err
+			results <- transfer
+		}()
+	}
+
+	for x := 0; x < n; x++ {
+		err := <-errs
+		require.NoError(t, err)
+
+		result := <-results
+		require.NotEmpty(t, result)
+
+		transfer := result.Transfer
+		require.NotEmpty(t, transfer)
+		require.Equal(t, account1.ID, transfer.FromAccountID)
+		require.Equal(t, account2.ID, transfer.ToAccountID)
+		require.Equal(t, amount, transfer.Amount)
+
+		require.NotZero(t, transfer.ID)
+		require.NotZero(t, transfer.CreatedAt)
+
+		_, err = store.GetTransfer(context.Background(), transfer.ID)
+		require.NoError(t, err)
+
+		fromEntry := result.FromEntry
+		require.NotEmpty(t, fromEntry)
+
+		require.Equal(t, fromEntry.AccountID, account1.ID)
+		require.Equal(t, fromEntry.Amount, -amount)
+		require.NotZero(t, fromEntry.ID)
+		require.NotZero(t, fromEntry.CreatedAt)
+
+		_, err = store.GetEntry(context.Background(), fromEntry.ID)
+		require.NoError(t, err)
+
+		toEntry := result.ToEntry
+		require.NotEmpty(t, toEntry)
+
+		require.Equal(t, toEntry.AccountID, account2.ID)
+		require.Equal(t, toEntry.Amount, amount)
+		require.NotZero(t, toEntry.ID)
+		require.NotZero(t, toEntry.CreatedAt)
+
+		_, err = store.GetEntry(context.Background(), toEntry.ID)
+		require.NoError(t, err)
+
+		// TODO: for later
+		// from_account := result.FromAccount
+		// require.NotEmpty(t, from_account)
+		//
+		// require.Equal(t, account1.ID, from_account.ID)
+		// require.Equal(t, account1.Currency, from_account.Currency)
+		//
+		//   _, err = store.GetAcc
+	}
+
+}
