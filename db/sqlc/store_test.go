3e9051b9139d34044feff56e1dd694acf325d211
Mrityunjay Saxena <14573967+mrityunjaygr8@users.noreply.github.com>
2022-11-03 17:01:20 +0530

    trying to avoid deadlocks by using predictable ordering
    Now addMoney operations are being ordered based on the ID of 2 accounts.
    
    Account with smaller ID is now always being done first

--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -104,3 +104,47 @@ func TestTransferTx(t *testing.T) {
 	require.Equal(t, account2.Balance+int64(n)*amount, updatedAccount2.Balance)
 
 }
+func TestTransferTxDeadlock(t *testing.T) {
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+
+	store := NewStore(testDb)
+
+	errs := make(chan error)
+
+	n := 10
+	amount := int64(10)
+
+	for x := 0; x < n; x++ {
+		fromAccountID := account1.ID
+		toAccountID := account2.ID
+
+		if x%2 == 0 {
+			fromAccountID = account2.ID
+			toAccountID = account1.ID
+		}
+		go func() {
+			_, err := store.TransferTx(context.Background(), TransferTxParams{
+				FromAccountID: fromAccountID,
+				ToAccountID:   toAccountID,
+				Amount:        amount,
+			})
+
+			errs <- err
+		}()
+	}
+
+	for x := 0; x < n; x++ {
+		err := <-errs
+		require.NoError(t, err)
+	}
+
+	updatedAccount1, err := testQueries.GetAccount(context.Background(), account1.ID)
+	require.NoError(t, err)
+	updatedAccount2, err := testQueries.GetAccount(context.Background(), account2.ID)
+	require.NoError(t, err)
+
+	require.Equal(t, account1.Balance, updatedAccount1.Balance)
+	require.Equal(t, account2.Balance, updatedAccount2.Balance)
+
+}
